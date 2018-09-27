12ea2f0816b2c20495ae66ed3910bb1a6ace1e6b
iris <iris@cs.williams.edu>
2018-09-27 18:00:15 -0400

    Gets all the co-authors for a given author

--- network_analysis.py
+++ network_analysis.py
@@ -9,30 +9,70 @@ network file to be visualized as a social network analysis.
 """
 import scholarly
 
+CONST_DELIMITER = "," # for CSV files
+CONST_YEAR = 2018
+articles = [] # titles of publications we've already considered (no double count)
+anonymized = {} # names of authors we're not including in the analysis
+
 
 def makeList(file_name):
     """ Reads in a list with a search query for Google Scholar on each line.
     This is intended to read in a list of author names, for authors that
     are members of our networks of interest.
 
-    :param file_name: name of file to open, one author name/query per line
+    :param file_name: name of CSV file to open, one author name/query per line,
+    first column should be the author's name
     :return: list of each author name/query
     """
     lines = [] 
     f = open(file_name)
     for line in f:
-        lines.append(line.strip())
+        all_cols = line.split(CONST_DELIMITER)
+        lines.append(all_cols[0].strip()) # assumes first column is author name
     return lines
+
+def get_coauthors(author_name="Iris Howley", num_years=10):
+    """ Given an author's name, find all their co-authors
+
+    :param author_name: the name of the author of interest
+    :param num_years: go back how many years?
+    :return: a list of all the co-authors
+    """
+    
+    # Retrieve the author's data, fill-in
+    search_query = scholarly.search_author(author_name)
+    author = next(search_query).fill()
+    #print(author)
+
+    # Print the titles of the author's publications
+    #print([pub.bib['title'] for pub in author.publications])
+
+    # yes, there's a more python way to do this...
+    for pub in author.publications:
+        pub.fill()
+        
+        # check to see if we've already counted this article
+        # and ensure it's not too old
+        title = pub.bib['title']
+        year = pub.bib['year']
+        if title not in articles and CONST_YEAR-year <= num_years:
+            articles.append(title)
+        
+            coauthors = pub.bib['author'].split(" and ")
+            print(coauthors)
         
 
 def example(author_name="Iris Howley"):
     """
     Example function from the scholarly website.
     'Here’s a quick example demonstrating how to retrieve an author’s 
-    profile then retrieve the titles of the papers that cite his most 
+    profile then retrieve the titles of the papers that cite hermost 
     popular (cited) paper.'
 
     https://pypi.org/project/scholarly/
+
+    :param author_name: Name of author to p[rint data for
+    :return: None
     """
     # Retrieve the author's data, fill-in, and print
     search_query = scholarly.search_author(author_name)
@@ -54,5 +94,5 @@ if __name__=='__main__':
     This is the main function. This is what is called when the file
     is run as a script (and not as a library or in interactive python)
     """
-    #pass # remove this later
-    example()
+    #example()
+    get_coauthors()
