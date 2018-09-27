4a787a2acdc641c641614639c49622d3f845c88b
iris <iris@cs.williams.edu>
2018-09-27 19:58:19 -0400

    needs to read from a file

--- network_analysis.py
+++ network_analysis.py
@@ -150,7 +150,7 @@ if __name__=='__main__':
 
     #test()
 
-    for author in author_pubs:
+    for author in make_list(CONST_FNAME):
         print("\tReversing: " + author + " --> " +str(author_pubs[author]))
         for value in author_pubs[author]:
             # if we haven't seen this title before, add it
